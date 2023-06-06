37628984fb499a384f13f1f79d8605c44d59f0c1
lmbsog0 <guillaume.sottas@liebherr.com>
2023-06-06 16:06:31 +0200

    add gRPC code generation

---
+++ generate_grpc.sh
@@ -0,0 +1,14 @@
+wget https://golang.org/dl/go1.20.4.linux-amd64.tar.gz
+sudo tar -xf go1.20.4.linux-amd64.tar.gz -C /usr/local
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v23.2/protoc-23.2-linux-x86_64.zip
+sudo unzip -o protoc-23.2-linux-x86_64.zip -d /usr/local bin/protoc
+sudo unzip -o protoc-23.2-linux-x86_64.zip -d /usr/local 'include/*'
+rm -f protoc-23.2-linux-x86_64.zip
+
+export PATH=$PATH:$HOME/go/bin
+
+protoc --go_out=./pkg/a2l/ --go_opt=paths=source_relative --go-grpc_out=./pkg/a2l/ --go-grpc_opt=paths=source_relative ./protobufs/*.proto
