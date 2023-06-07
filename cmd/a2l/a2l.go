53de6b3b97dcbdf058a99743ffa30a3c26e981ef
lmbsog0 <guillaume.sottas@liebherr.com>
2023-06-07 12:27:58 +0200

    update DLL API

--- cmd/a2l/a2l.go
+++ cmd/a2l/a2l.go
@@ -5,20 +5,22 @@ import (
 	"context"
 	"fmt"
 	"github.com/antlr4-go/antlr/v4"
-	"google.golang.org/grpc"
-	"os"
-
 	"github.com/sauci/a2l-grpc/pkg/a2l"
 	"github.com/sauci/a2l-grpc/pkg/a2l/parser"
-	"log"
+	"google.golang.org/grpc"
 	"net"
+	"sync"
 )
 
-func NewTreeFromString(a2lContent string) (tree *a2l.RootNodeType, err error) {
-	input := antlr.NewInputStream(a2lContent)
+type grpcA2LImplType struct {
+	a2l.UnimplementedA2LServer
+}
+
+func (s *grpcA2LImplType) GetTree(_ context.Context, request *a2l.GetTreeRequest) (_ *a2l.RootNodeType, err error) {
+	input := antlr.NewInputStream(request.GetA2L())
 	lexer := parser.NewA2LLexer(input)
-	stream := antlr.NewCommonTokenStream(lexer, 0)
-	p := parser.NewA2LParser(stream)
+	tokenStream := antlr.NewCommonTokenStream(lexer, 0)
+	p := parser.NewA2LParser(tokenStream)
 	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
 	p.BuildParseTrees = true
 
@@ -26,62 +28,58 @@ func NewTreeFromString(a2lContent string) (tree *a2l.RootNodeType, err error) {
 
 	antlr.ParseTreeWalkerDefault.Walk(listener, p.A2lFile())
 
-	tree = listener.Tree()
-
-	return tree, err
+	return listener.Tree(), err
 }
 
-type grpcA2LImplType struct {
-	a2l.UnimplementedA2LServer
-}
+var serverMutex sync.Mutex
 
-func (s *grpcA2LImplType) GetTree(_ context.Context, request *a2l.GetTreeRequest) (result *a2l.RootNodeType, err error) {
-	var tree *a2l.RootNodeType
+var server *grpc.Server
 
-	var file *os.File
-	file, err = os.OpenFile("C:\\git\\gomodparser\\a2l_parser.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	defer func() { _ = file.Close() }()
-
-	logger := log.New(file, "C:\\git\\gomodparser\\a2l_parser.log", log.LstdFlags)
-
-	logger.Printf("GetTree request: %s", request.String())
-
-	if tree, err = NewTreeFromString(request.GetA2L()); err == nil {
-		result = tree
-	} else {
-		logger.Print(err)
-	}
-
-	return result, err
-}
-
-//export NewServer
-func NewServer(port C.int) {
+//export Create
+func Create(port C.int) (result C.int) {
 	var err error
-	var lis net.Listener
-	var file *os.File
+	var listener net.Listener
 
-	file, err = os.OpenFile("C:\\git\\gomodparser\\a2l_parser.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	defer func() { _ = file.Close() }()
+	serverMutex.Lock()
+	defer serverMutex.Unlock()
 
-	logger := log.New(file, "C:\\git\\gomodparser\\a2l_parser.log", log.LstdFlags)
-
-	logger.Printf("start listening on port %v", port)
-
-	if lis, err = net.Listen("tcp", fmt.Sprintf(":%v", port)); err == nil {
-
-		s := grpc.NewServer(grpc.MaxRecvMsgSize(100*1024*1024), grpc.MaxSendMsgSize(100*1024*1024))
-
-		a2l.RegisterA2LServer(s, &grpcA2LImplType{})
-
-		go func() {
-			if err = s.Serve(lis); err != nil {
-				logger.Print(err)
-			}
-		}()
+	if server != nil {
+		result = 1
 	} else {
-		logger.Print(err)
+		result = 0
 	}
+
+	if result == 0 {
+		if listener, err = net.Listen("tcp", fmt.Sprintf(":%v", port)); err == nil {
+			server = grpc.NewServer(grpc.MaxRecvMsgSize(100*1024*1024), grpc.MaxSendMsgSize(100*1024*1024))
+
+			a2l.RegisterA2LServer(server, &grpcA2LImplType{})
+
+			go func() {
+				err = server.Serve(listener)
+			}()
+		}
+	}
+
+	return result
+}
+
+//export Close
+func Close() (result C.int) {
+	serverMutex.Lock()
+	defer serverMutex.Unlock()
+
+	if server == nil {
+		result = 1
+	} else {
+		server.GracefulStop()
+
+		server = nil
+
+		result = 0
+	}
+
+	return result
 }
 
 func main() {}
