25538caee15b00bffa7c21063fe8193558fa8ab9
lmbsog0 <guillaume.sottas@liebherr.com>
2023-06-16 13:24:25 +0200

    add JSON to tree conversion method, cleanup API

--- cmd/a2l/a2l.go
+++ cmd/a2l/a2l.go
@@ -9,11 +9,17 @@ import (
 	"github.com/sauci/a2l-grpc/pkg/a2l/parser"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/encoding/protojson"
+	"io"
 	"net"
+	"os"
 	"sync"
 )
 
 func getTreeFromString(a2lString string) (result *a2l.RootNodeType, err error) {
+	stdErr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
 	lexer := parser.NewA2LLexer(antlr.NewInputStream(a2lString))
 	tokenStream := antlr.NewCommonTokenStream(lexer, 0)
 	p := parser.NewA2LParser(tokenStream)
@@ -24,6 +30,15 @@ func getTreeFromString(a2lString string) (result *a2l.RootNodeType, err error) {
 
 	antlr.ParseTreeWalkerDefault.Walk(listener, p.A2lFile())
 
+	_ = w.Close()
+	out, _ := io.ReadAll(r)
+
+	os.Stderr = stdErr
+
+	if len(out) != 0 {
+		err = fmt.Errorf("%s", string(out))
+	}
+
 	return listener.Tree(), err
 }
 
@@ -31,36 +46,53 @@ type grpcA2LImplType struct {
 	a2l.UnimplementedA2LServer
 }
 
-func (s *grpcA2LImplType) GetTree(_ context.Context, request *a2l.GetTreeRequest) (result *a2l.GetTreeResponse, err error) {
+func (s *grpcA2LImplType) GetTreeFromA2L(_ context.Context, request *a2l.A2LRequest) (result *a2l.TreeResponse, err error) {
 	var tree *a2l.RootNodeType
-	var internalErr error
+	var parseError error
 
-	result = &a2l.GetTreeResponse{}
+	result = &a2l.TreeResponse{}
 
-	if tree, internalErr = getTreeFromString(request.GetA2L()); internalErr == nil {
+	if tree, parseError = getTreeFromString(request.GetA2L()); parseError == nil {
 		result.Tree = tree
 	} else {
-		errString := internalErr.Error()
+		errString := parseError.Error()
 		result.Error = &errString
 	}
 
 	return result, err
 }
 
-func (s *grpcA2LImplType) GetJSONTree(_ context.Context, request *a2l.RootNodeType) (result *a2l.GetJSONTreeResponse, err error) {
+func (s *grpcA2LImplType) GetJSONFromTree(_ context.Context, request *a2l.TreeRequest) (result *a2l.JSONResponse, err error) {
 	var rawData []byte
-	var internalErr error
+	var parseError error
 
-	result = &a2l.GetJSONTreeResponse{}
+	result = &a2l.JSONResponse{}
 
-	if rawData, internalErr = protojson.Marshal(request); internalErr == nil {
+	if rawData, parseError = protojson.Marshal(request.Tree); parseError == nil {
 		result.Json = string(rawData)
 	} else {
-		errString := internalErr.Error()
+		errString := parseError.Error()
 		result.Error = &errString
 	}
 
 	return result, err
+}
+
+func (s *grpcA2LImplType) GetTreeFromJSON(_ context.Context, request *a2l.JSONRequest) (result *a2l.TreeResponse, err error) {
+	var parseError error
+
+	result = &a2l.TreeResponse{Tree: &a2l.RootNodeType{}}
+
+	if parseError = protojson.Unmarshal([]byte(request.Json), result.Tree); parseError != nil {
+		errString := parseError.Error()
+		result.Error = &errString
+	}
+
+	return result, err
+}
+
+func (s *grpcA2LImplType) GetA2LFromTree(_ context.Context, request *a2l.TreeRequest) (result *a2l.A2LResponse, err error) {
+	panic("not implemented yet...")
 }
 
 var serverMutex sync.Mutex
@@ -83,7 +115,7 @@ func Create(port C.int) (result C.int) {
 
 	if result == 0 {
 		if listener, err = net.Listen("tcp", fmt.Sprintf(":%v", port)); err == nil {
-			server = grpc.NewServer(grpc.MaxRecvMsgSize(100*1024*1024), grpc.MaxSendMsgSize(100*1024*1024))
+			server = grpc.NewServer(grpc.MaxRecvMsgSize(200*1024*1024), grpc.MaxSendMsgSize(200*1024*1024))
 
 			a2l.RegisterA2LServer(server, &grpcA2LImplType{})
 
@@ -114,4 +146,10 @@ func Close() (result C.int) {
 	return result
 }
 
-func main() {}
+func main() {
+	Create(3333)
+
+	for {
+		select {}
+	}
+}
