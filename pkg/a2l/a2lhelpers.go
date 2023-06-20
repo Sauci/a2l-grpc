65a89b756fb7f5f3f5e48a72a5acf31288ae5557
lmbsog0 <guillaume.sottas@liebherr.com>
2023-06-20 14:40:49 +0200

    ensure non-nil pointer access when parsing partial input file

--- pkg/a2l/a2lhelpers.go
+++ pkg/a2l/a2lhelpers.go
@@ -13,25 +13,27 @@ func a2lIntToIntType(integerValue parser.IIntegerValueContext) (result *IntType)
 	var base = uint32(0)
 	var rawString string
 
-	if integerValue.HEX() != nil {
-		base = 16
-		rawString = integerValue.HEX().GetText()
-		rawString = strings.Replace(strings.Replace(rawString, "0X", "", -1), "0x", "", -1)
-	} else if integerValue.INT() != nil {
-		base = 10
-		rawString = integerValue.INT().GetText()
-	} else {
-		panic(fmt.Errorf("unimplemented int conversion"))
-	}
-
-	if tmpResult, err = strconv.ParseInt(rawString, int(base), 64); err == nil {
-		result = &IntType{
-			Value: int32(tmpResult),
-			Base:  base,
-			Size:  uint32(len(rawString)),
+	if integerValue != nil {
+		if integerValue.HEX() != nil {
+			base = 16
+			rawString = integerValue.HEX().GetText()
+			rawString = strings.Replace(strings.Replace(rawString, "0X", "", -1), "0x", "", -1)
+		} else if integerValue.INT() != nil {
+			base = 10
+			rawString = integerValue.INT().GetText()
+		} else {
+			panic(fmt.Errorf("unimplemented int conversion"))
 		}
-	} else {
-		panic(err)
+
+		if tmpResult, err = strconv.ParseInt(rawString, int(base), 64); err == nil {
+			result = &IntType{
+				Value: int32(tmpResult),
+				Base:  base,
+				Size:  uint32(len(rawString)),
+			}
+		} else {
+			panic(err)
+		}
 	}
 
 	return result
@@ -43,25 +45,27 @@ func a2lLongToLongType(integerValue parser.IIntegerValueContext) (result *LongTy
 	var base = uint32(0)
 	var rawString string
 
-	if integerValue.HEX() != nil {
-		base = 16
-		rawString = integerValue.HEX().GetText()
-		rawString = strings.Replace(strings.Replace(rawString, "0X", "", -1), "0x", "", -1)
-	} else if integerValue.INT() != nil {
-		base = 10
-		rawString = integerValue.INT().GetText()
-	} else {
-		panic(fmt.Errorf("unimplemented int conversion"))
-	}
-
-	if tmpResult, err = strconv.ParseInt(rawString, int(base), 64); err == nil {
-		result = &LongType{
-			Value: int32(tmpResult),
-			Base:  base,
-			Size:  uint32(len(rawString)),
+	if integerValue != nil {
+		if integerValue.HEX() != nil {
+			base = 16
+			rawString = integerValue.HEX().GetText()
+			rawString = strings.Replace(strings.Replace(rawString, "0X", "", -1), "0x", "", -1)
+		} else if integerValue.INT() != nil {
+			base = 10
+			rawString = integerValue.INT().GetText()
+		} else {
+			panic(fmt.Errorf("unimplemented int conversion"))
 		}
-	} else {
-		panic(err)
+
+		if tmpResult, err = strconv.ParseInt(rawString, int(base), 64); err == nil {
+			result = &LongType{
+				Value: int32(tmpResult),
+				Base:  base,
+				Size:  uint32(len(rawString)),
+			}
+		} else {
+			panic(err)
+		}
 	}
 
 	return result
@@ -73,25 +77,27 @@ func numericToLongType(integerValue parser.INumericValueContext) (result *LongTy
 	var base = uint32(0)
 	var rawString string
 
-	if integerValue.HEX() != nil {
-		base = 16
-		rawString = integerValue.HEX().GetText()
-		rawString = strings.Replace(strings.Replace(rawString, "0X", "", -1), "0x", "", -1)
-	} else if integerValue.INT() != nil {
-		base = 10
-		rawString = integerValue.INT().GetText()
-	} else {
-		panic(fmt.Errorf("unimplemented int conversion"))
-	}
-
-	if tmpResult, err = strconv.ParseInt(rawString, int(base), 64); err == nil {
-		result = &LongType{
-			Value: int32(tmpResult),
-			Base:  base,
-			Size:  uint32(len(rawString)),
+	if integerValue != nil {
+		if integerValue.HEX() != nil {
+			base = 16
+			rawString = integerValue.HEX().GetText()
+			rawString = strings.Replace(strings.Replace(rawString, "0X", "", -1), "0x", "", -1)
+		} else if integerValue.INT() != nil {
+			base = 10
+			rawString = integerValue.INT().GetText()
+		} else {
+			panic(fmt.Errorf("unimplemented int conversion"))
 		}
-	} else {
-		panic(err)
+
+		if tmpResult, err = strconv.ParseInt(rawString, int(base), 64); err == nil {
+			result = &LongType{
+				Value: int32(tmpResult),
+				Base:  base,
+				Size:  uint32(len(rawString)),
+			}
+		} else {
+			panic(err)
+		}
 	}
 
 	return result
@@ -104,77 +110,73 @@ func floatToFloatType(integerValue parser.INumericValueContext) (result *FloatTy
 	var integralSign string
 	var exponentSign string
 
-	result = &FloatType{}
+	if integerValue != nil {
+		result = &FloatType{}
 
-	processedString := integerValue.GetText()
-	originalString := integerValue.GetText()
+		processedString := integerValue.GetText()
+		originalString := integerValue.GetText()
 
-	if strings.HasPrefix(processedString, "+") {
-		integralSign = "+"
-		result.IntegralSign = &integralSign
-		processedString = strings.TrimLeft(processedString, "+")
-	} else if strings.HasPrefix(processedString, "-") {
-		integralSign = "-"
-		result.IntegralSign = &integralSign
-		processedString = strings.TrimLeft(processedString, "-")
-	}
+		if strings.HasPrefix(processedString, "+") {
+			integralSign = "+"
+			result.IntegralSign = &integralSign
+			processedString = strings.TrimLeft(processedString, "+")
+		} else if strings.HasPrefix(processedString, "-") {
+			integralSign = "-"
+			result.IntegralSign = &integralSign
+			processedString = strings.TrimLeft(processedString, "-")
+		}
 
-	if strings.Contains(processedString, ".") {
-		split = strings.Split(processedString, ".")
-		result.IntegralSize = uint32(len(split[0]))
+		if strings.Contains(processedString, ".") {
+			split = strings.Split(processedString, ".")
+			result.IntegralSize = uint32(len(split[0]))
 
-		if len(split) > 1 {
-			decimalString := split[1]
-			if strings.Contains(decimalString, "e") {
-				split = strings.Split(decimalString, "e")
-				result.DecimalSize = uint32(len(split[0]))
-				exponentString := split[1]
+			if len(split) > 1 {
+				decimalString := split[1]
+				if strings.Contains(decimalString, "e") {
+					split = strings.Split(decimalString, "e")
+					result.DecimalSize = uint32(len(split[0]))
+					exponentString := split[1]
 
-				if strings.HasPrefix(exponentString, "+") {
-					exponentSign = "+"
-					result.ExponentSign = &exponentSign
-					exponentString = strings.TrimLeft(exponentString, "+")
-				} else if strings.HasPrefix(exponentString, "-") {
-					exponentSign = "-"
-					result.ExponentSign = &exponentSign
-					exponentString = strings.TrimLeft(exponentString, "-")
+					if strings.HasPrefix(exponentString, "+") {
+						exponentSign = "+"
+						result.ExponentSign = &exponentSign
+						exponentString = strings.TrimLeft(exponentString, "+")
+					} else if strings.HasPrefix(exponentString, "-") {
+						exponentSign = "-"
+						result.ExponentSign = &exponentSign
+						exponentString = strings.TrimLeft(exponentString, "-")
+					}
+
+					result.ExponentSize = uint32(len(exponentString))
+				} else {
+					result.DecimalSize = uint32(len(split[1]))
 				}
-
-				result.ExponentSize = uint32(len(exponentString))
-			} else {
-				result.DecimalSize = uint32(len(split[1]))
 			}
 		}
-	}
 
-	if tmpResult, err = strconv.ParseFloat(originalString, 64); err == nil {
-		result.Value = float32(tmpResult)
-	} else {
-		panic(err)
+		if tmpResult, err = strconv.ParseFloat(originalString, 64); err == nil {
+			result.Value = float32(tmpResult)
+		} else {
+			panic(err)
+		}
 	}
 
 	return result
 }
 
 func identifierToIdentType(identifierValue parser.IIdentifierValueContext) (result *IdentType) {
-	return &IdentType{Value: identifierValue.GetText()}
+	if identifierValue != nil {
+		result = &IdentType{Value: identifierValue.GetText()}
+	}
+
+	return result
 }
 
 func a2lStringToStringType(stringValue parser.IStringValueContext) (result *StringType) {
-	rawString := stringValue.GetText()
+	if stringValue != nil {
+		rawString := stringValue.GetText()
+		result = &StringType{Value: rawString[1 : len(rawString)-1]}
+	}
 
-	return &StringType{Value: rawString[1 : len(rawString)-1]}
+	return result
 }
-
-//func a2mlTagToString(tagValue parser.ITagValueContext) (result tagType) {
-//	return tagType(tagValue.GetText())
-//}
-//
-//func a2mlIdentifierToString(identifierValue parser.IIdentifierValueContext) (result IdentType) {
-//	return IdentType{Value: identifierValue.GetText()}
-//}
-//
-//func a2mlStringToString(stringValue parser.IStringValueContext) (result StringType) {
-//	rawString := stringValue.GetText()
-//	return StringType{Value: rawString[1 : len(rawString)-1]}
-//}
