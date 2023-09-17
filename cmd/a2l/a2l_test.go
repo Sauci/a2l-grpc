3f8f945952094b95d981acf85bbd48c080dc673e
lmbsog0 <guillaume.sottas@liebherr.com>
2023-09-17 13:22:32 +0200

    sort elements naturally ("2" < "10")

--- cmd/a2l/a2l_test.go
+++ cmd/a2l/a2l_test.go
@@ -107,3 +107,115 @@ func Test_GetJSONFromTree(t *testing.T) {
 		})
 	}
 }
+
+func TestGrpcA2LImplType_GetA2LFromTree(t *testing.T) {
+	type testCaseType struct {
+		request  *a2l.A2LFromTreeRequest
+		response *a2l.A2LResponse
+	}
+
+	for _, tc := range []testCaseType{
+		{
+			request: &a2l.A2LFromTreeRequest{
+				Tree: &a2l.RootNodeType{
+					PROJECT: &a2l.ProjectType{
+						Name:           &a2l.IdentType{Value: "_"},
+						LongIdentifier: &a2l.StringType{Value: "_"},
+						MODULE: []*a2l.ModuleType{
+							{
+								Name:           &a2l.IdentType{Value: "_"},
+								LongIdentifier: &a2l.StringType{Value: "_"},
+								CHARACTERISTIC: []*a2l.CharacteristicType{
+									{
+										Name:           &a2l.IdentType{Value: "c[1]"},
+										LongIdentifier: &a2l.StringType{Value: "_"},
+										Type:           "VALUE",
+										Address:        &a2l.LongType{Value: 0x00000000},
+										Deposit:        &a2l.IdentType{Value: "_"},
+										MaxDiff:        &a2l.FloatType{Value: 0.0},
+										Conversion:     &a2l.IdentType{Value: "_"},
+										LowerLimit:     &a2l.FloatType{Value: 0.0},
+										UpperLimit:     &a2l.FloatType{Value: 0.0},
+									},
+									{
+										Name:           &a2l.IdentType{Value: "b[1]"},
+										LongIdentifier: &a2l.StringType{Value: "_"},
+										Type:           "VALUE",
+										Address:        &a2l.LongType{Value: 0x00000000},
+										Deposit:        &a2l.IdentType{Value: "_"},
+										MaxDiff:        &a2l.FloatType{Value: 0.0},
+										Conversion:     &a2l.IdentType{Value: "_"},
+										LowerLimit:     &a2l.FloatType{Value: 0.0},
+										UpperLimit:     &a2l.FloatType{Value: 0.0},
+									},
+									{
+										Name:           &a2l.IdentType{Value: "a[0]"},
+										LongIdentifier: &a2l.StringType{Value: "_"},
+										Type:           "VALUE",
+										Address:        &a2l.LongType{Value: 0x00000000},
+										Deposit:        &a2l.IdentType{Value: "_"},
+										MaxDiff:        &a2l.FloatType{Value: 0.0},
+										Conversion:     &a2l.IdentType{Value: "_"},
+										LowerLimit:     &a2l.FloatType{Value: 0.0},
+										UpperLimit:     &a2l.FloatType{Value: 0.0},
+									},
+									{
+										Name:           &a2l.IdentType{Value: "c[0]"},
+										LongIdentifier: &a2l.StringType{Value: "_"},
+										Type:           "VALUE",
+										Address:        &a2l.LongType{Value: 0x00000000},
+										Deposit:        &a2l.IdentType{Value: "_"},
+										MaxDiff:        &a2l.FloatType{Value: 0.0},
+										Conversion:     &a2l.IdentType{Value: "_"},
+										LowerLimit:     &a2l.FloatType{Value: 0.0},
+										UpperLimit:     &a2l.FloatType{Value: 0.0},
+									},
+									{
+										Name:           &a2l.IdentType{Value: "b[10]"},
+										LongIdentifier: &a2l.StringType{Value: "_"},
+										Type:           "VALUE",
+										Address:        &a2l.LongType{Value: 0x00000000},
+										Deposit:        &a2l.IdentType{Value: "_"},
+										MaxDiff:        &a2l.FloatType{Value: 0.0},
+										Conversion:     &a2l.IdentType{Value: "_"},
+										LowerLimit:     &a2l.FloatType{Value: 0.0},
+										UpperLimit:     &a2l.FloatType{Value: 0.0},
+									},
+								},
+							},
+						},
+					},
+				},
+				Indent: baseTypeToPointer[uint32](1),
+				Sorted: baseTypeToPointer[bool](true),
+			},
+			response: &a2l.A2LResponse{
+				A2L: []byte(`/begin PROJECT _ "_"
+ /begin MODULE _ "_"
+  /begin CHARACTERISTIC a[0] "_" VALUE 0x0 _ 0 _ 0 0
+  /end CHARACTERISTIC
+  /begin CHARACTERISTIC b[1] "_" VALUE 0x0 _ 0 _ 0 0
+  /end CHARACTERISTIC
+  /begin CHARACTERISTIC b[10] "_" VALUE 0x0 _ 0 _ 0 0
+  /end CHARACTERISTIC
+  /begin CHARACTERISTIC c[0] "_" VALUE 0x0 _ 0 _ 0 0
+  /end CHARACTERISTIC
+  /begin CHARACTERISTIC c[1] "_" VALUE 0x0 _ 0 _ 0 0
+  /end CHARACTERISTIC
+ /end MODULE
+/end PROJECT`),
+				Error: nil,
+			},
+		},
+	} {
+		t.Run("", func(t *testing.T) {
+			grpc := grpcA2LImplType{}
+
+			if response, err := grpc.GetA2LFromTree(nil, tc.request); err == nil {
+				assert.Equal(t, tc.response, response)
+			} else {
+				t.Fatal(err)
+			}
+		})
+	}
+}
